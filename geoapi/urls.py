24537e47237c8856c66a5ce505c12f536816f480
jstout90 <jordan.stout90@gmail.com>
2019-07-28 11:13:12 -0400

    make it better

--- geoapi/urls.py
+++ geoapi/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
 from geoapi import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 app_name = 'geoapi'
 
 urlpatterns = [
     path('', views.GeoLocation.as_view(), name='geodata'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
