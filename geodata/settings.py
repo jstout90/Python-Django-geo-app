0885aa245034b126d78ab519d2cd8d110b3dae43
jstout90 <jordan.stout90@gmail.com>
2019-07-28 11:10:35 -0400

    make it better

--- geodata/settings.py
+++ geodata/settings.py
@@ -13,7 +13,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://shrouded-bastion-95553.herokuapp.com/']
 
